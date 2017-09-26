c82ed4328b515d2e2fa3a31e8afb544004ae667f
seksu <jarukit.suchat@gmail.com>
2017-09-26 23:18:56 +0700

    Add files via upload

---
+++ Claymore's Dual Ethereum+Decred_Siacoin_Lbry_Pascal AMD+NVIDIA GPU Miner v10.0/start.bat
@@ -0,0 +1,6 @@
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+EthDcrMiner64.exe -epool eu1.ethermine.org:4444 -ewal 0xd62533ab4fc3dbb25c8c6aa22b9e74541efb29f5.hello1 -epsw x
